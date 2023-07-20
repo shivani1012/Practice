e30bbc6664342e2c9ade16cf78aa86a71e613c4f
shivani1012 <61676362+shivani1012@users.noreply.github.com>
2023-07-20 16:57:17 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// // Number
+// let a = 5;
+// let b = 5.5;
+// console.log(typeof(a));
+
+// String
+// let c = "5";
+// let d = "5.5";
+
+// //undefined
+// let e;
+// console.log(e);
+
+// // null
+// let f = null;
+// console.log(f);
+
+// // BigInt
+// let g = 6374673647637647364n;
+// console.log(g);
+
+// // symbol
+// Symbol();
+
+// Object
+// const obj = {
+//   name: 'Shivani',
+//   gender: 'Female',
+//   age: 28
+// }
+// console.log(obj.name);
+
+// Array
+// const array = [1, 2, 3, "4"];
+// console.log(array[0]);
---
+++ src/variables.js
@@ -0,0 +1,4 @@
+var a = 5;
+var a = 6;
+a = 7;
+console.log(a);
